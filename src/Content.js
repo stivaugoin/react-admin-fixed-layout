7510a90891f8af3862e55f8ce742f45f76dc4832
Steve Bourgoin <stiv.aw@gmail.com>
2017-01-26 16:00:14 -0500

    Make it responsive

--- src/Content.js
+++ src/Content.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
-import addEvent from './lib/add-event';
 
 class LayoutContent extends Component {
   componentDidMount() {
     let pageHeight = document.body.offsetHeight;
-    const navbarHeight = document.getElementsByClassName('react-admin-fixed-layout-navbar')[0].offsetHeight || 0;
-    const headerHeight = document.getElementsByClassName('react-admin-fixed-layout-header')[0].offsetHeight || 0;
+    const navbar = document.getElementsByClassName('react-admin-fixed-layout-navbar')[0];
+    const navbarHeight = navbar && navbar.offsetHeight || 0;
+    const header = document.getElementsByClassName('react-admin-fixed-layout-header')[0];
+    const headerHeight = header && header.offsetHeight || 0;
     const content = document.getElementsByClassName('react-admin-fixed-layout-content')[0];
     content.style.height = `${pageHeight - (navbarHeight + headerHeight)}px`;
 
-    addEvent(window, 'resize', function () {
+    window.addEventListener('resize', () => {
       pageHeight = document.body.offsetHeight;
       content.style.height = `${pageHeight - (navbarHeight + headerHeight)}px`;
-    });
+    }, false);
   }
 
   render() {
